e79a877afa41f9cb1d3e7616b22ec8d3308d0810
Medina <dnt.1554@gmail.com>
2016-10-26 23:26:04 +0000

    aula par e impar

---
+++ parimpar.c
@@ -0,0 +1,24 @@
+/*Faça um programa em C para ler um numero inteiro e dizer
+se é par ou impar
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    {
+        printf("Diga um número");
+        scanf("%d", &num);
+        
+        if( (num % 2)==0 )//condição verdadeira
+            {
+                printf("\nÉ par!");
+            }
+        else//condição falsa
+            {
+                printf("\nÉ impar!");
+            }
+    }
+    
+}
